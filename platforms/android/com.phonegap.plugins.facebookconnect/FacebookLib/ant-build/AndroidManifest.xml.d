f422d2a225830e49b218148ab1944d82e60952a5
lcrodriguez <leonardo.rodriguez@nybblegroup.com>
2015-03-02 12:39:47 -0300

    Updated APP DEVELOPER

---
+++ platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/leo/dev/appbuild/appdeveloper/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/ant-build/AndroidManifest.xml : \
+/Users/leo/dev/appbuild/appdeveloper/platforms/android/com.phonegap.plugins.facebookconnect/FacebookLib/AndroidManifest.xml \
